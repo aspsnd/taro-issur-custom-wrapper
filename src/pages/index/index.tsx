7fd3facd15b6116a51a165d520f3b4ee6514eefe
aspsnd <2546697613@qq.com>
2024-12-19 16:59:16 +0800

    init

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,11 @@
+import { View, CustomWrapper } from "@tarojs/components";
+
+export default function Index() {
+  return (
+    <View className="page">
+      <CustomWrapper>
+        <n-view />
+      </CustomWrapper>
+    </View>
+  );
+}
